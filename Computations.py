b0128a6f9eaf9e066587cb91113da49bde1dd346
Octavia <ocosby2024@fau.edu>
2024-09-07 13:10:23 -0400

    import the data and tidy/transform it

---
+++ Computations.py
@@ -0,0 +1,19 @@
+#import the necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#assign the daata to a new variable
+data = pd.read_csv("telescope_data.csv")
+
+#read the data
+data.head(201)
+
+#remove the last column because it is a string and we won't be able to compute accurately
+train = data.iloc[:,:-1]
+print(train)
+
+#convert data to numpy
+train1 = train.to_numpy()
+print(train1)
+
